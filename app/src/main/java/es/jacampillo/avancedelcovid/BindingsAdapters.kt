6c33e0e0d0903a90c00ebe3c417f7d5ca60f745d
Jose Antonio <campi1112@gmail.com>
2020-04-06 18:30:58 +0200

    muestra fecha en barra de título

--- app/src/main/java/es/jacampillo/avancedelcovid/BindingsAdapters.kt
+++ app/src/main/java/es/jacampillo/avancedelcovid/BindingsAdapters.kt
@@ -9,6 +9,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import es.jacampillo.avancedelcovid.models_api_response.Pais
 import es.jacampillo.avancedelcovid.ui.main.PaisesAdapter
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("imgUrl")
 fun bindImage(imageView: ImageView, urlImg: String?) {
@@ -38,4 +40,11 @@ fun bindPositivos(tv : TextView, pais: Pais?){
         tv.text = String.format("Positivos al test: %d", pais.cases)
     }
 
+}
+
+    //SimpleDateFormat("yyyy.MM.dd HH:mm")
+fun Long.toDateFormat(): String{
+    val date = Date(this)
+    val format = SimpleDateFormat("HH:mm dd.MM.yy ")
+        return format.format(date)
 }
--- app/src/main/java/es/jacampillo/avancedelcovid/ui/main/MainFragment.kt
+++ app/src/main/java/es/jacampillo/avancedelcovid/ui/main/MainFragment.kt
@@ -8,33 +8,35 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import es.jacampillo.avancedelcovid.databinding.MainFragmentBinding
+import es.jacampillo.avancedelcovid.toDateFormat
 
 class MainFragment : Fragment() {
 
-    private val viewmodel : MainViewModel by lazy {
+    private val viewmodel: MainViewModel by lazy {
         val activity = requireNotNull(this.activity)
         val factory = MainViewModel.Factory(activity.application)
         ViewModelProvider(this, factory)
             .get(MainViewModel::class.java)
     }
 
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        viewmodel.paises.observe(viewLifecycleOwner, Observer {
-//            viewModelAdapter?.submitList(it)
-//        })
-//    }
-//
-//    private var viewModelAdapter: PaisesAdapter? = null
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val binding = MainFragmentBinding.inflate(inflater)
 
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewmodelo = viewmodel
         binding.paisesRecyclerView.adapter = PaisesAdapter()
 
+        viewmodel.paises.observe(viewLifecycleOwner, Observer {
+            activity?.title = it.get(0).updated?.toDateFormat()
+        })
+
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 }
+
+
